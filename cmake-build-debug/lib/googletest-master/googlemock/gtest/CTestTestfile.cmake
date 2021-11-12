ef0ed59397da9acb0ed7fb22e42afbfab5ada2f9
Silkking7 <danieljseidler@gmail.com>
2021-11-12 10:38:41 -0300

    [DEL] cmake-build-debug from git

--- cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/silkking/Workspace/Facultad/algoI_tp2/lib/googletest-master/googletest
-# Build directory: /home/silkking/Workspace/Facultad/algoI_tp2/cmake-build-debug/lib/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
